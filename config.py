ab7146a6cf262a041de9bf9353522dea7ffe99d2
Lukman Harahap <swaruppanda047@gmail.com>
2026-03-28 23:57:50 +0530

    Update bot token, alive pic, and repo URL

--- config.py
+++ config.py
@@ -12,13 +12,13 @@ API_HASH = getenv("API_HASH", "152e4c828fbc897e927f42894ae9c9f4") #optional
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 OWNER_ID = 7396578515
 MONGO_URL = getenv("mongodb+srv://mailnewtest1234_db_user:urps1q1RuW4ebVHQ@newclusteruserbot.fob21b9.mongodb.net/")
-BOT_TOKEN = getenv("BOT_TOKEN", "8201603613:AAGJEdjTHtWW8rdPHoZwtdQoovzkonJgS2M")
-ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/3c52a01057865f7511168.jpg')
+BOT_TOKEN = getenv("BOT_TOKEN", "8429717262:AAHBQ7nqzSc0LibQrOaxM337UFXxtQclhVY")
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://i.ibb.co/RkB0mshb/photo-2026-03-28-18-26-18-7622374658888695856.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
+REPO_URL = getenv("REPO_URL", "https://github.com/Hellobot-cmd/Eryx-USERBOT.git")
 BRANCH = getenv("BRANCH", "master") #don't change
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
